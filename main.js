c1348962bd20155b3e172c503ee75030b86ebc5a
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-08-16 17:24:06 +0200

    added working hamburger menu

---
+++ main.js
@@ -0,0 +1,33 @@
+const hamburger = document.querySelector('.hamburger');
+const lines = document.querySelectorAll('.line');
+const menuLinks = document.querySelector('.menu');
+const links = document.querySelectorAll('.menu__item');
+
+//włączanie i wyłączanie menu po kliknięciu w hamburger
+const toggleHamburgerClasses = () => {
+  menuLinks.classList.toggle('open');
+  links.forEach(link => {
+    link.classList.toggle('fade');
+  });
+  lines.forEach(line => {
+    line.classList.toggle('makeCross');
+  });
+};
+
+//zamykanie menu po kliknięciu w link - obsługa
+const closeMenu = () => {
+  menuLinks.classList.toggle('open');
+  links.forEach(link => {
+    link.classList.toggle('fade');
+  });
+  lines.forEach(line => {
+    line.classList.toggle('makeCross');
+  });
+};
+
+//działający hamburger
+hamburger.addEventListener('click', toggleHamburgerClasses);
+//zamykanie menu po kliknięciu w link
+links.forEach(link => {
+  link.addEventListener('click', closeMenu);
+});
