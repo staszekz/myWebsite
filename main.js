e8150bc70f8f7c153764272d1c2801e4d824c3a4
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-11-18 12:39:58 +0100

    working desctop menu

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const menuLinks = document.querySelector('.menu');
 const links = document.querySelectorAll('.menu__item');
 
 //włączanie i wyłączanie menu po kliknięciu w hamburger
+
 const toggleHamburgerClasses = () => {
 	menuLinks.classList.toggle('open');
+
 	links.forEach(link => {
 		link.classList.toggle('fade');
 	});
@@ -14,15 +16,19 @@ const toggleHamburgerClasses = () => {
 	});
 };
 
+console.log('szeroosć', window.innerWidth);
 //zamykanie menu po kliknięciu w link - obsługa
 const closeMenu = () => {
-	menuLinks.classList.toggle('open');
-	links.forEach(link => {
-		link.classList.toggle('fade');
-	});
-	lines.forEach(line => {
-		line.classList.toggle('makeCross');
-	});
+	if (window.innerWidth < 769) {
+		//żeby nie właczało się po kliknięciu w link na szerokim oknie
+		menuLinks.classList.toggle('open');
+		links.forEach(link => {
+			link.classList.toggle('fade');
+		});
+		lines.forEach(line => {
+			line.classList.toggle('makeCross');
+		});
+	}
 };
 
 //działający hamburger
