9f35952717f342e0e7c822697726db5ec5b9fff5
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2021-01-08 14:14:10 +0100

    added function to send message to sender

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,11 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
-
-// const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const SENDGRID_API_KEY = functions.config().sendgrid.key;
 const TEMPLATE_ID = functions.config().sendgrid.template
+const TEMPLATE_TO_SENDER = functions.config().sendgrid.templatetosender;
 const sendGridEmail = require('@sendgrid/mail');
 
 sendGridEmail.setApiKey(SENDGRID_API_KEY);
@@ -29,6 +19,22 @@ exports.staszek_ovh_form = functions.firestore.document('/mails/{mailsId}').onCr
 			name: messageData.name,
 			email: messageData.email,
 			location: messageData.location,
+			message: messageData.message,
+		},
+	};
+	return sendGridEmail
+		.send(msg)
+		.then(() => console.log('email sent'))
+		.catch((error:Error) => { throw new Error(error.toString()) });
+});
+
+exports.staszek_ovh_form_to_sender = functions.firestore.document('/mails/{mailsId}').onCreate((snap:any) => {
+	const messageData = snap.data();
+	const msg = {
+		to: messageData.email,
+		from: 'ktulu.inc@gmail.com',
+		templateId: TEMPLATE_TO_SENDER,
+		dynamic_template_data: {
 			message: messageData.message,
 		},
 	};
